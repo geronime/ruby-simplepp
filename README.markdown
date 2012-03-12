ba0c8b1cfd05726ed452846eb6250a1432abed75
Jiri Nemecek <nemecek.jiri@gmail.com>
2012-03-13 00:00:51 +0100

    ruby-simplepp first revision.

--- README.markdown
+++ README.markdown
@@ -2,10 +2,45 @@
 
 + [github project] (https://github.com/geronime/ruby-simplepp)
 
-ruby-simplepp is a module to simple string pretty print of hash/array structures
+ruby-simplepp is a module to simple string pretty print of hash/array structures.
 
+## Usage:
 
-## License
+    require 'ruby-simplepp'
+    pretty = SimplePP.pp(struct, indent_level=0, indent_str='  ')
+
++ `indent_level` specifies starting indentation level (internally used
+  for recursion as well)
++ indentation string can be redefined with `indent_str` (defaults to two spaces)
+
+## Example:
+
+    irb> puts SimplePP.pp struct
+    {
+      "resource" => "anywhere",
+      "q"        => "Some query",
+      "search"   => "fulltext",
+      "limit"    => 20,
+      "details"  => false,
+      "foo"      => nil,
+      "bar"      => "baz",
+      "result"   => {
+        "search_time" => 0.532,
+        "item_count"  => 1,
+        "items"       => [
+          {
+            "name" => "Item name",
+            "url"  => "http://foo.bar.baz"
+          }
+        ]
+      }
+    }
+
+## Changelog:
+
++ __0.0.1__: first revision of simple pretty print
+
+## License:
 
 ruby-simplepp is copyright (c)2012 Jiri Nemecek, and released under the terms
 of the MIT license. See the LICENSE file for the gory details.
--- lib/ruby-simplepp.rb
+++ lib/ruby-simplepp.rb
@@ -2,6 +2,52 @@
 require 'ruby-simplepp/version'
 
 module SimplePP
-	# Your code goes here...
-end
+
+	def self.pp struct, indent_level=0, indent_str='  '
+		str, indent, indent_in = '',
+			indent_str * indent_level, indent_str * (indent_level + 1)
+		if struct.kind_of?(Array)
+			str += "[\n"
+			lastidx = struct.length - 1
+			struct.each_with_index{|v, i|
+				str += indent_in + inner_pp(v, indent_level, indent_str)
+				str += ',' unless i == lastidx
+				str += "\n"
+			}
+			str += indent + "]"
+		elsif struct.kind_of?(Hash)
+			key_maxl = 0
+			struct.each_key{|k| l = k.to_s.length and l > key_maxl and key_maxl = l }
+			key_maxl += 2
+			str += "{\n"
+			idx, lastidx = 0, struct.length - 1
+			struct.each_pair{|k, v|
+				str += indent_in + sprintf("%-#{key_maxl}s => %s",
+						"\"#{k.to_s}\"", inner_pp(v, indent_level, indent_str)
+					)
+				str += ',' unless idx == lastidx
+				idx += 1
+				str += "\n"
+			}
+			str += indent + "}"
+		else
+			str += inner_pp struct, indent_level, indent_str
+		end
+		str
+	end
+
+	private
+
+	def self.inner_pp val, indent_level, indent_str
+		val.kind_of?(Numeric) ||
+				val.kind_of?(TrueClass) || val.kind_of?(FalseClass) ?
+			val.to_s :
+			val.kind_of?(NilClass) ?
+				"nil" :
+				val.kind_of?(Array) || val.kind_of?(Hash) ?
+					pp(val, indent_level + 1) :
+					"\"#{val.to_s}\""
+	end
+
+end # SimplePP
 
